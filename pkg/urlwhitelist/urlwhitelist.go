51a5d6d615a685d1b5a8bb2082f78e5dd1763470
xiaoyuan <lony0116@163.com>
2025-06-18 21:21:35 +0800

    add urlwhite

--- pkg/urlwhitelist/urlwhitelist.go
+++ pkg/urlwhitelist/urlwhitelist.go
@@ -5,8 +5,19 @@ var Business = map[string]int{
 	"/pb.BusinessExt/TwitterSignIn":          1,
 	"/pb.BusinessExt/GetTwitterAuthorizeURL": 2,
 	"/pb.BusinessExt/WalletSignIn":           3,
+	"/pb.BusinessExt/SearchTwitterUser":      4,
+
+	"/pb.BusinessExt/CreateToken": 5,
+	"/pb.BusinessExt/GetToken":    6,
 }
 
 var Logic = map[string]int{
-	"/pb.LogicExt/RegisterDevice": 0,
+	"/pb.LogicExt/RegisterDevice":      0,
+	"/pb.LogicExt/CreateChatRoom":      1,
+	"/pb.LogicExt/GetChatRoom":         2,
+	"/pb.LogicExt/GetChatRooms":        3,
+	"/pb.LogicExt/JoinChatRoom":        4,
+	"/pb.LogicExt/LeaveChatRoom":       5,
+	"/pb.LogicExt/GetChatRoomMembers":  6,
+	"/pb.LogicExt/SendChatRoomMessage": 7,
 }
