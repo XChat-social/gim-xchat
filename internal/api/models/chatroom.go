9884746be44403f1ee37ede97267972d09875252
xiaoyuan <lony0116@163.com>
2025-06-19 12:48:31 +0800

    update chatroom model

--- internal/api/models/chatroom.go
+++ internal/api/models/chatroom.go
@@ -8,14 +8,20 @@ import (
 
 // ChatRoom 聊天室模型
 type ChatRoom struct {
-	gorm.Model
-	RoomID         int64  `gorm:"uniqueIndex;not null" json:"room_id"` // 聊天室ID
-	Name           string `gorm:"size:50;not null" json:"name"`        // 聊天室名称
-	AvatarURL      string `gorm:"size:255" json:"avatar_url"`          // 头像URL
-	Introduction   string `gorm:"size:500" json:"introduction"`        // 简介
-	MaxMemberCount int32  `gorm:"not null" json:"max_member_count"`    // 最大成员数
-	Extra          string `gorm:"size:1024" json:"extra"`              // 附加字段
-	CreatorID      int64  `gorm:"not null" json:"creator_id"`          // 创建者ID
+	ID             uint      `gorm:"primarykey" json:"id"`                                  // 自增主键
+	RoomID         int64     `gorm:"uniqueIndex;not null" json:"room_id"`                   // 聊天室ID
+	Name           string    `gorm:"size:50;not null" json:"name"`                          // 聊天室名称
+	AvatarURL      string    `gorm:"size:255;not null" json:"avatar_url"`                   // 聊天室头像
+	Introduction   string    `gorm:"size:255;not null" json:"introduction"`                 // 聊天室简介
+	CreatorID      int64     `gorm:"not null;index" json:"creator_id"`                      // 创建者ID
+	UserNum        int32     `gorm:"default:0;not null" json:"user_num"`                    // 当前人数
+	OnlineCount    int32     `gorm:"default:0;not null" json:"online_count"`                // 在线人数
+	MemberCount    int32     `gorm:"default:0;not null" json:"member_count"`                // 成员总数
+	MaxMemberCount int32     `gorm:"default:1000;not null" json:"max_member_count"`         // 最大成员数量
+	Status         int8      `gorm:"default:1;not null" json:"status"`                      // 状态 1:正常 2:已关闭
+	Extra          string    `gorm:"size:1024;default:'';not null" json:"extra"`            // 附加属性
+	CreateTime     time.Time `gorm:"default:CURRENT_TIMESTAMP;not null" json:"create_time"` // 创建时间
+	UpdateTime     time.Time `gorm:"default:CURRENT_TIMESTAMP;not null" json:"update_time"` // 更新时间
 }
 
 // ChatRoomMember 聊天室成员模型
